08b24d0fbd0a043fda466da2a251b9f9ed09f2e9
gzy1992123 <872598520@qq.com>
2019-08-28 09:21:04 +0800

    无效修改

--- GYBaseViewCategory/AViewController.m
+++ GYBaseViewCategory/AViewController.m
@@ -48,14 +48,4 @@
     NSLog(@"释放了AVIEWCONTROLLER");
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
